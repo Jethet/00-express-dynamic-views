6bd8cc0ff3b22cc955231d8e57e8dcee008c6551
Jethet <henriette.hettinga@gmail.com>
2020-02-06 11:47:11 +0100

    Add html, selections

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const app = express();
 const port = 3000
 
 
-
-
 // MIDDLEWARE
 app.use(express.static(__dirname + '/public'));
 // set handlebars:
@@ -22,12 +20,35 @@ app.get('/', (req, res, next) => {
 app.get('/student', (req, res, next) => {
 
     const data = {      // This can be used as parameter in the render function
-        name: 'Helen',
-        bootcamp: 'WebDev BCN'
-    };
-    res.render('student', data);
-});
-
+        name: "Ironhacker",
+        bootcamp: "<h3>WebDev - Barcelona</h3>",
+        message: "Rocking it!",
+        address: "Carrer de Lull",
+        cities: ["Miami", "Madrid", "Barcelona", "Paris", "México", "Berlin"],
+        countries: [
+        "Spain",
+        "France",
+        "Germany",
+        "Portugal",
+        "United States",
+        "Brazil"
+        ],
+        info: { name: "Ironhacker", campus: "Barcelona", year: 2019 },
+        addressObj: {
+        street: "Sardenya",
+        number: 100
+        }
+  };
+        // address: ""
+        // address: 0
+        // address: undefined
+        // address: false
+        // address: null
+        // address: []
+        // address: NaN
+        
+        res.render('student', data);
+    });
 
 
 app.listen(port, () => {
